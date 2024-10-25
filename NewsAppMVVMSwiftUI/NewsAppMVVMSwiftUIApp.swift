ed95a79eaea4b0648eda7267abbb135b5725ae51
Chron1um635 <chron1um522@gmail.com>
2024-10-25 14:59:42 +0300

    Initial Commit

---
+++ NewsAppMVVMSwiftUI/NewsAppMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppMVVMSwiftUIApp.swift
+//  NewsAppMVVMSwiftUI
+//
+//  Created by Максим Назаров on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
