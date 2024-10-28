97c391922e45f1c9c0091bef0dab06c01eada4cf
Chron1um635 <chron1um522@gmail.com>
2024-10-28 15:59:31 +0300

    setup data manager

--- NewsAppMVVMSwiftUI/Services/DataManager.swift
+++ NewsAppMVVMSwiftUI/Services/DataManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class DataManager {
+    
+    static let shared = DataManager()
+    
+    private let userDefaults = UserDefaults()
+    
+    private init() {}
+    
+    func saveCounter(for news: String, with counter: Int) {
+        userDefaults.set(counter, forKey: news)
+    }
+    
+    func loadCount(for news: String) -> Int {
+        userDefaults.integer(forKey: news)
+    }
+}
