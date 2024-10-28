6d0339e2b69344b4a090790ffbc1ce786d387e8c
Chron1um635 <chron1um522@gmail.com>
2024-10-28 15:59:13 +0300

    Setup details view

--- NewsAppMVVMSwiftUI/NewsDetailsView/NewsDetailsView.swift
+++ NewsAppMVVMSwiftUI/NewsDetailsView/NewsDetailsView.swift
@@ -6,13 +6,58 @@
 //
 
 import SwiftUI
+import WebKit
+
 
 struct NewsDetailsView: View {
+    
+    @StateObject var viewModel: NewsDetailsViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 20)
+             {
+                NewsImageView(
+                    imageData: viewModel.newsImageData,
+                    size: CGSize(width: 300, height: 200),
+                    cornerRadius: 30
+                )
+                Text(viewModel.newsTitle)
+                     .font(.title)
+                     .bold()
+                     .multilineTextAlignment(.center)
+                 Text(viewModel.newsDescription)
+                      .font(.headline)
+                 Text(viewModel.newsAuthor)
+                 NavigationLink(destination: WebView(url: viewModel.newsUrl)) {
+                     Text(viewModel.newsUrl?.absoluteString ?? "")
+                 }
+                 
+            }
+        }
+        .onAppear {
+            viewModel.increaseCount()
+        }
+        .padding()
     }
 }
 
 #Preview {
-    NewsDetailsView()
+    NewsDetailsView(viewModel: NewsDetailsViewModel(news: Article.getArticle()))
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let wkWebView = WKWebView()
+        if let url = url {
+            let request = URLRequest(url: url)
+            wkWebView.load(request)
+        }
+        return wkWebView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+    }
 }
