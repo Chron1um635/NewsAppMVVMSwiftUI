e99e314a666520b24d9232ae097a2bf022ae3ca7
Chron1um635 <chron1um522@gmail.com>
2024-10-28 16:00:28 +0300

    setup news list view model

--- NewsAppMVVMSwiftUI/NewsListView/NewsListViewModel.swift
+++ NewsAppMVVMSwiftUI/NewsListView/NewsListViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+final class NewsListViewModel: ObservableObject {
+    @Published var news: [NewsDetailsViewModel] = []
+    
+    func fetchData() {
+        NetworkManager.shared.fetchNews(from: Link.mainUrl.url) { [unowned self] result in
+            switch result {
+            case .success(let data):
+                news = data.articles.map { NewsDetailsViewModel(news: $0) }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
