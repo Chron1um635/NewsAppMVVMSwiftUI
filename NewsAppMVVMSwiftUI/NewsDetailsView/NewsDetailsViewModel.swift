78c0854c7d6832ddda45f786cde4eae691f41233
Chron1um635 <chron1um522@gmail.com>
2024-10-28 15:58:53 +0300

    Setup details view model

--- NewsAppMVVMSwiftUI/NewsDetailsView/NewsDetailsViewModel.swift
+++ NewsAppMVVMSwiftUI/NewsDetailsView/NewsDetailsViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+final class NewsDetailsViewModel: ObservableObject {
+    private let news: Article
+    
+    var newsTitle: String {
+        news.title
+    }
+    
+    @Published var newsImageData = Data()
+    
+    @Published var counter: Int {
+        didSet {
+            DataManager.shared.saveCounter(for: news.title, with: counter)
+        }
+    }
+    
+    var newsDescription: String {
+        news.description ?? "No description"
+    }
+    
+    var newsAuthor: String {
+        "Author - \(news.author ?? "No author")"
+    }
+    
+    var newsUrl: URL? {
+        URL(string: news.url)
+    }
+    
+    init(news: Article) {
+        self.news = news
+        counter = DataManager.shared.loadCount(for: news.title)
+    }
+    
+    func fetchImageData() {
+        NetworkManager.shared.fetchImageData(from: news.urlToImage ?? "") { [unowned self] data in
+            newsImageData = data
+        }
+    }
+    
+    func increaseCount() {
+        counter += 1
+    }
+}
